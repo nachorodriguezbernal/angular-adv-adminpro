3b322a2c968a58f41cc0c473c86f986ec40e3b8b
nacho rodriguez <nacho.rodriguezbe@gmail.com>
2020-11-29 17:56:09 +0100

    Fin sección 4 - Router

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,30 @@
+import { AppRoutingModule } from './../app-routing.module';
+import { SharedModule } from './../shared/shared.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProgressComponent } from './progress/progress.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent,
+  ],
+  exports: [
+    DashboardComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    AppRoutingModule
+  ]
+})
+export class PagesModule { }
