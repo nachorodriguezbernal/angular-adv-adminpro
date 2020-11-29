f4d6cd889f27656b1c1f2227448329a8a2e5e4f4
nacho rodriguez <nacho.rodriguezbe@gmail.com>
2020-11-29 13:39:10 +0100

    fin seccion 3 - diseño

---
+++ src/app/pages/pages.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import { HeaderComponent } from '../shared/header/header.component';
+import { SidebarComponent } from '../shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from '../shared/breadcrumbs/breadcrumbs.component';
+
+@Component({
+  selector: 'app-pages',
+  templateUrl: './pages.component.html',
+  styles: [
+  ]
+})
+export class PagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
